cf0fb5f22a8c5c3ae4058ad86f1545648ba67fde
Pardeep <pardeep@sweetlegs.ca>
2021-10-28 08:54:35 -0700

    initial project setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose  from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors());
+
+// mongoDB
+const CONNECTION_URL = 'mongodb+srv://pardeep_mern_stack:5ZKd5aiyv3MXEHWy@cluster0.n66oq.mongodb.net/mern_stack_jwt?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message))
+
+
+
